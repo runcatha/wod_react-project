05b6c2e090888de9b9bdfa9e92f534ac606b6dc5
Cathleen Runde <cathleenrunde@Cathleens-MBP.home>
2021-08-26 15:25:15 -0700

    add column

--- client/src/components/AllOfTheWODs.jsx
+++ client/src/components/AllOfTheWODs.jsx
@@ -8,21 +8,23 @@ function AllOfTheWODs(props) {
   console.log(wods)
   return (
     <>
-      <div className='wod-list-title'>
-        <h1>WOD list</h1>
-      </div>
-      <div className='wod-container'>
-        {
-          wods.map((workOuts, index) => (
-            <React.Fragment key={index} >
-              <div className='wod-list'>
-                <Link to={`/WODSpecs/${workOuts.id}`} key={workOuts.id}>
-                  <button id='name'>{workOuts.fields.name}</button>
-                </Link>
-              </div>
-            </React.Fragment>
-          ))
-        }
+      <div className='all-of-the-everything'>
+        <div className='wod-list-title'>
+          <h1>WOD list</h1>
+        </div>
+        <div className='wod-container'>
+          {
+            wods.map((workOuts, index) => (
+              <React.Fragment key={index} >
+                <div className='wod-list'>
+                  <Link to={`/WODSpecs/${workOuts.id}`} key={workOuts.id}>
+                    <button id='name'>{workOuts.fields.name}</button>
+                  </Link>
+                </div>
+              </React.Fragment>
+            ))
+          }
+        </div>
       </div>
     </>
   )
